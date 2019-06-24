9331d65c5514d48b73f84e73d9e092bf00b424e7
Abimael Espinal <abimaelespinal@gmail.com>
2019-06-24 00:58:33 -0400

    added deck component

--- App.js
+++ App.js
@@ -2,13 +2,13 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Ball from './src/components/Ball';
-
+import Deck from './src/components/Deck'
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Ball/>
+      <Deck/>
       </View>
     );
   }
---
+++ src/components/Deck.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {View,Text,Animated,StyleSheet} from 'react-native';
+
+class Deck extends Component {
+    state = {  }
+
+    
+
+    render() { 
+        return ( 
+            
+            <View/>
+         );
+    }
+}
+ 
+export default Deck;
